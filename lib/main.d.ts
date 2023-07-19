e063c28d9bcb8b1c42c824612f090fd1ce10c1a5
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2023-07-18 21:31:49 -0400

    primera version

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,14 +1,13 @@
 import React from "react";
-declare abstract class StateHandler<T> {
+declare class Controller<T> {
     state?: T;
     readonly setState: React.Dispatch<React.SetStateAction<T>>;
-    protected instanceCreated?: () => void;
-    constructor(hs: HandlerSetter<T>);
+    constructor(hs: ControllerSetter<T>);
 }
-declare abstract class StateHandlerState<T> extends StateHandler<T> {
+declare abstract class ControllerWState<T> extends Controller<T> {
     abstract state: T;
 }
-declare type HandlerSetter<T> = [T, React.Dispatch<React.SetStateAction<T>>];
-declare function useStateHandler<T, H extends StateHandler<T>>(handlerClass: new (s: HandlerSetter<T>, state?: T) => H, initial_value: T | (() => T)): [T, H];
-declare function useStateHandler<T, H extends StateHandler<T>>(handlerClass: new (s: HandlerSetter<T>, state?: T) => H, initial_value?: T | (() => T)): [H extends StateHandlerState<T> ? T : T | undefined, H];
-export { StateHandler, useStateHandler, HandlerSetter };
+declare type ControllerSetter<T> = [T, React.Dispatch<React.SetStateAction<T>>];
+declare function useController<T, H>(fun: (c: Controller<T>) => H, initial_value: T | (() => T)): [T, Controller<T> & H];
+declare function useController<T, H>(fun: (c: Controller<T>) => H, initial_value?: T | (() => T)): [H extends ControllerWState<T> ? T : T | undefined, Controller<T> & H];
+export { Controller, useController, ControllerSetter };
